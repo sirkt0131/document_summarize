8690387a4b1f426406d93c19329bf3a2a65fe4fc
Shuichiro Kito <s.kitou@gmail.com>
2024-01-31 10:52:51 +0900

    Change  'Japanese' as default.

--- data/selection.py
+++ data/selection.py
@@ -3,4 +3,4 @@ class GetList:
         return ('gpt-3.5-turbo-16k', 'gpt-3.5-turbo', 'gpt-4-1106-preview', 'gpt-4-32k', 'gpt-4')
 
     def get_language_list():
-        return ('English', 'Japanese')
+        return ('Japanese', 'English')
